b6998678446b4d6a19af2d068b4671117d15b0d1
Md Muhib Ullah Habib <mdmuhibullahhabib6348@gmail.com>
2025-07-23 21:02:00 +0600

    nav

---
+++ src/Authprovider/Authprovider.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Authprovider = () => {
+  return (
+    <div>Authprovider</div>
+  )
+}
+
+export default Authprovider
--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import Router from './Routes/Router.jsx'
-import Authprovider from "./Provider/Authprovider.jsx";
+// import Authprovider from './Authprovider/Authprovider.jsx'
 import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
-import Router from "./Routes/Router.jsx";
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+        {/* <Authprovider> */}
+      <QueryClientProvider client={queryClient}>
     <div className="max--screen-xl mx-auto">
       <RouterProvider router={Router}></RouterProvider>
     </div>
+    </QueryClientProvider>
+    {/* </Authprovider> */}
   </StrictMode>,
 )
