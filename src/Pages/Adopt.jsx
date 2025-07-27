eba222756d06d93384b382f32eefc4ab249a2dff
mdmuhibullahhabib <mdmuhibullahhabib6348@gmail.com>
2025-07-27 23:53:41 +0600

    adopt

--- src/Pages/Adopt.jsx
+++ src/Pages/Adopt.jsx
@@ -41,7 +41,7 @@ const Adopt = () => {
     setAdoptedPets(updatedPets);
     localStorage.setItem('adoptedPets', JSON.stringify(updatedPets));
   };
-  co
+  c
 
   const handleChange = (e) => {
     const { name, value } = e.target;
