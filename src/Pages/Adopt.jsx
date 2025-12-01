7b04167060acab120d955d1c528c7388e3eab786
mdmuhibullahhabib <mdmuhibullahhabib6348@gmail.com>
2025-12-01 20:08:42 +0600

    adopt

--- src/Pages/Adopt.jsx
+++ src/Pages/Adopt.jsx
@@ -29,9 +29,8 @@ const Adopt = () => {
         id: '3',
         name: 'Luna',
         breed: 'Persian Cat',
-        age: '3 years',
-        image: 'https://placekitten.com/500/300'
-      }
+   
+      
      
     ];
     setAdoptedPets(storedPets);
