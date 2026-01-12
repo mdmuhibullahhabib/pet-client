20ffeb6bba8ce773874f131f43addc6c373c2ad4
mdmuhibullahhabib <mdmuhibullahhabib6348@gmail.com>
2026-01-12 21:48:49 +0600

    adopt

--- src/Pages/Adopt.jsx
+++ src/Pages/Adopt.jsx
@@ -32,6 +32,13 @@ const Adopt = () => {
         age: '1 year',
         image: 'https://placedog.net/500?id=2'
       }
+      {
+        id: '3',
+        name: 'Luna',
+        breed: 'Persian Cat',
+        age: '1 year',
+        image: 'https://placedog.net/500?id=2'
+      }
      
     ];
     setAdoptedPets(storedPets);
