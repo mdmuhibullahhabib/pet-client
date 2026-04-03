977046627fc14149ff95cf4cd225af34c342d11d
mdmuhibullahhabib <mdmuhibullahhabib6348@gmail.com>
2026-04-04 00:14:40 +0600

    data

--- src/Pages/Adopt.jsx
+++ src/Pages/Adopt.jsx
@@ -32,10 +32,6 @@ const Adopt = () => {
         age: '1 year',
         image: 'https://placedog.net/500?id=2'
       },
-        breed: 'Beagle',
-        age: '1 year',
-        image: 'https://placedog.net/500?id=2'
-      },
  
 
 
